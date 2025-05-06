ff9767906885d72e71cbdc3402f7efb58af07133
Sameera Chathuranga <sameeranicho@gmail.com>
2025-05-06 10:57:40 +0400

    update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,25 +2,9 @@
 import io, { Socket } from "socket.io-client";
 import { dispatch, useSelector } from "@/redux/store";
 import React, { useEffect, useRef, useState } from "react";
-import { set, useForm } from "react-hook-form";
-import { Dashboard } from "../components/DashboardTile";
-import FormProvider from "../components/Form/hook-form/FormProvider";
-import ImageBg from "../../../public/images/image 1.png";
 
-import {
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
-  Transition,
-} from "@headlessui/react";
-import {
-  ArchiveBoxXMarkIcon,
-  ChevronDownIcon,
-  PencilIcon,
-  Square2StackIcon,
-  TrashIcon,
-} from "@heroicons/react/16/solid";
+import { Dashboard } from "../components/DashboardTile";
+
 import { DashboardTile } from "../constants/dashboard";
 import { getAllPart } from "@/redux/slices/dashboard";
 import { PartData, PartWithProject } from "@/interfaces/part";
@@ -29,6 +13,8 @@ import { INotification } from "@/interfaces/notification";
 import { getSettings } from "@/redux/slices/setting";
 import { ISetting } from "@/interfaces/setting";
 import { DefaultSetting } from "../constants/setting";
+import LinearProgress from "@mui/material/LinearProgress";
+import { Box } from "@mui/material";
 
 const DashboardPage = () => {
   const socketRef = useRef<Socket | null>(null);
@@ -64,12 +50,10 @@ const DashboardPage = () => {
     const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL as string);
 
     socket.on("connect", () => {
-      console.log("Connected to socket:", socket.id);
       socket.emit("register-tv", "tv-lobby");
     });
 
     socket.on("DASHBOARD", (payload) => {
-      console.log("Received payload:", payload);
       if (payload.type === "UPDATE") {
         dispatch(getAllPart(formattedDate));
       } else if (payload.type === "ADD") {
@@ -85,9 +69,8 @@ const DashboardPage = () => {
     });
 
     socket.on("NOTIFICATION", (payload) => {
-      console.log("Received payload:", payload);
       setImmediateNotification(payload.notification);
-      console.log("Notification payload:", payload.notification);
+
       setTimeout(() => {
         setImmediateNotification(null);
       }, payload.notification.duration * 1000);
@@ -136,27 +119,26 @@ const DashboardPage = () => {
     }
   }, [Part, setting?.setting.gridCount]);
 
-  // useEffect(() => {
-  //   if (setting?.setting?.duration ?? 60000 > 0) {
-  //     setProgress(0);
+  useEffect(() => {
+    const durationInSeconds = setting?.setting?.duration ?? 60; // default 60 seconds
 
-  //     const increment = 100 / (setting?.setting?.duration ?? 60000 / 100);
-  //     const timer = setInterval(() => {
-  //       setProgress((prevProgress) => {
-  //         const nextProgress = prevProgress + increment;
-  //         if (nextProgress >= 100) {
-  //           clearInterval(timer);
-  //           return 100;
-  //         }
-  //         return nextProgress;
-  //       });
-  //     }, 100);
+    if (durationInSeconds > 0) {
+      const durationInMs = durationInSeconds * 1000;
+      const intervalMs = 100; // update every 100ms
+      const increment = (100 * intervalMs) / durationInMs;
 
-  //     return () => clearInterval(timer);
-  //   }
-  // }, [setting?.setting?.duration]);
+      setProgress(0); // reset when duration changes
 
-  console.log("se:", setting?.setting?.duration);
+      const timer = setInterval(() => {
+        setProgress((prevProgress) => {
+          const nextProgress = prevProgress + increment;
+          return nextProgress >= 100 ? 0 : nextProgress;
+        });
+      }, intervalMs);
+
+      return () => clearInterval(timer);
+    }
+  }, [setting?.setting?.duration]);
 
   useEffect(() => {
     if (partData.length === 0) return;
@@ -198,10 +180,10 @@ const DashboardPage = () => {
     }, (setting?.setting?.duration ?? 60) * 1000);
 
     return () => clearInterval(interval);
-  }, [setting?.setting.gridCount!, currentProject, partData]);
+  }, [setting?.setting.gridCount!, currentProject, partData, currentPosition]);
 
   return (
-    <div className="flex flex-col h-screen w-screen bg-[#1E1D1D]">
+    <div className="flex flex-col h-screen w-screen bg-black">
       {immediateNotification && (
         <div className="fixed top-0 left-0 right-0 z-50 p-4 bg-gray-800 text-white h-screen">
           <div className="flex items-center justify-between">
@@ -218,11 +200,14 @@ const DashboardPage = () => {
       )}
 
       <div>
+        <Box sx={{ width: "100%" }}>
+          <LinearProgress variant="determinate" value={progress} />
+        </Box>
         <h1 className="text-white font-bold 3xl:text-5xl text-xl text-center py-5">
           {Part.length > 0 &&
-            Part[currentProject].projectId.name +
+            Part[currentProject]?.projectId?.name +
               " - " +
-              new Date(Part[currentProject].parts[0].createdAt ?? "")
+              new Date(Part[currentProject]?.parts[0]?.createdAt ?? "")
                 .toISOString()
                 .split("T")[0]}
         </h1>
@@ -243,7 +228,7 @@ const DashboardPage = () => {
               partName={item.name}
               tagLines={item.tagLines}
               onHold={item.onHold}
-              progress={progress}
+              progress={0}
             />
           ))}
       </div>
